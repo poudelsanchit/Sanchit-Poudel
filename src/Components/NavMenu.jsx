6c6d3e90d09224841376029272e56058eea4f3fc
Sanchit Poudel <snchtpdl12@gmail.com>
2023-06-17 20:59:30 +0545

    NavBar completed

--- src/Components/NavMenu.jsx
+++ src/Components/NavMenu.jsx
@@ -2,21 +2,40 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { CgMenuRight } from 'react-icons/cg';
 import { IoCloseOutline } from 'react-icons/io5';
+import { useState } from 'react';
 
 const NavMenu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    console.log(isOpen)
     return (
         <>
-            <header className='fixed w-screen   pt-5 top-0    md:bg-bg-primary md:w-screen md:fixed md:pb-5 md:z-50 '>
-                <nav className=' flex justify-end mr-5 mt-0  md:justify-center md:mt-0'>
-                    <div className=" absolute top-0 right-0 mt-5 mr-6 text-3xl cursor-pointer z-20"><CgMenuRight /></div>
-                    <div className='rounded-lg  items-end flex flex-col z-50   md:gap-10 bg-box md:flex md:bg-bg-primary md:z-50 md:flex-row'>
-                        <IoCloseOutline className='flex flex-col float-right text-end text-2xl' />
+
+            <header className='fixed w-screen   pt-5 top-0 bg-bg-primary shadow-sm md:bg-bg-primary md:w-screen md:fixed md:pb-5 md:z-50 '>
+
+                <nav className='items-baseline flex flex-row mt-0 md:justify-evenly'>
+                    <div className='flex justify-start align-middle text-3xl pl-5'>Sanchit</div>
+
+                    <div className='hidden rounded-lg  items-end  flex-col z-50 gap-4 md:flex md:gap-10  md:bg-bg-primary md:z-50 md:flex-row ' >
                         <NavLink to={'/'}><li>Home</li></NavLink>
                         <NavLink to={'/about'}><li>About</li></NavLink>
                         <NavLink to={'/projects'}> <li>Projects</li></NavLink>
                         <NavLink to={'/contact'}><li>Contact</li></NavLink>
                     </div>
+                    {isOpen ?
+                        <div className='transition-all ease-out duration-300	 absolute flex flex-col gap-2 rounded-md right-0 z-50 bg-box mt-1 mr-6 md:hidden'>
+                            <div className="close-icon"><IoCloseOutline className='close-icon text-4xl cursor-pointer md:hidden' onClick={() => setIsOpen((value) => false)} /></div>
+                            <NavLink className='pr-4 pl-4' to={'/'}><li>Home</li></NavLink>
+                            <NavLink className='pr-4 pl-4' to={'/about'}><li>About</li></NavLink>
+                            <NavLink className='pr-4 pl-4' to={'/projects'}> <li>Projects</li></NavLink>
+                            <NavLink className='pr-4 pl-4' to={'/contact'}><li>Contact</li></NavLink>
 
+                        </div>
+                        :
+                        <div className=" absolute top-0 right-0 mt-6 mr-6 text-3xl cursor-pointer z-50 md:hidden" onClick={() => setIsOpen((value) => true)} ><CgMenuRight /></div>
+
+
+                    }
 
                 </nav >
 
