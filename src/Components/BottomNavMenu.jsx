981975e846d3fa1f6191ccc5bbb5f04a903f0fca
Sanchit Poudel <snchtpdl12@gmail.com>
2023-07-06 00:56:12 +0545

    feat: :zap: Bottom Navbar Icons

--- src/Components/BottomNavMenu.jsx
+++ src/Components/BottomNavMenu.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { BsInstagram, BsFacebook, BsGithub } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
+import { FiGithub } from 'react-icons/fi';
+import { FaFacebookSquare } from 'react-icons/fa';
+
 
 
 
@@ -10,8 +13,8 @@ const BottomNavMenu = () => {
             <div className='flex flex-col text-sm justify-evenly bottom-0 w-screen bg-bottom-nav-background p-5  md:flex-row gap-5'>
                 <div className='flex flex-row gap-10 text-lg order-2 md:order-1'>
                     <BsInstagram className='cursor-pointer' />
-                    <BsFacebook className='cursor-pointer' />
-                    <BsGithub className='cursor-pointer' />
+                    <FaFacebookSquare className='cursor-pointer' />
+                    <FiGithub className='cursor-pointer' />
 
                 </div>
                 <div className='text-xs md:text-sm order-1 md:order-2'>Designed with 💜 and coded with frustrations</div>
