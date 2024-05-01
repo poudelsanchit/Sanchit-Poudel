55ad96c05fb906b2d318ee120f2448146b6c5240
Sanchit poudel <snchtpdl12@gmail.com>
2024-05-01 13:43:33 +0545

    update TechStack.jsx

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -3,7 +3,7 @@ const TechStack = React.forwardRef((props,ref) => {
 
     return (
       <>
-        <div className="w-full flex justify-center text-txtprimary " ref={ref}>
+        <div className="w-full flex justify-center text-txtprimary hidden " ref={ref}>
           <div className="flex  flex-col  gap-5 min-h-screen pt-20 w-full z-50">
             <div className="tracking-[0.2em] font-semibold text-xl flex  ">
             <div className="w-8/12 ">Tech Stack</div>
